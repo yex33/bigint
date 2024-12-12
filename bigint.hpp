0d62463e25edd60c5bb7e968d18f4abeb245e2f1
Xunzhou Ye <yex33@mcmaster.ca>
2024-12-12 09:48:09 -0500

    start on bigint ctor

---
+++ bigint.hpp
@@ -0,0 +1,27 @@
+#include <cstdint>
+#include <string_view>
+#include <vector>
+
+#include "doctest.h"
+
+class bigint {
+  bool sign;
+  std::vector<uint32_t> n;
+
+public:
+  bigint() : sign(false), n(std::vector<uint32_t>(1, 0)) {};
+  bigint(std::string_view sv);
+};
+
+inline bigint::bigint(std::string_view sv) {
+  for (auto it = sv.rbegin(); it != sv.rend(); it++) {
+  }
+}
+
+#ifdef DOCTEST_LIBRARY_INCLUDED
+
+TEST_CASE("[bigint] reeee") {
+  CHECK(1 == 1);
+}
+
+#endif
