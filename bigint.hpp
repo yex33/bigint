bea8010ce88823ea77b4407efc2bd1cde9f3b20f
Xunzhou Ye <yex33@mcmaster.ca>
2024-12-13 13:40:54 -0500

    start on parsing string input

--- bigint.hpp
+++ bigint.hpp
@@ -1,4 +1,8 @@
 #include <cstdint>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <string>
 #include <string_view>
 #include <vector>
 
@@ -17,12 +21,22 @@ public:
 
 inline bigint::bigint(std::string_view sv) {
   for (auto it = sv.rbegin(); it != sv.rend(); it++) {
+    if (*it == '-' && it != sv.rbegin()) {
+      sign = true;
+      continue;
+    }
+    uint32_t digit = static_cast<uint32_t>(std::stoul(std::string{*it}));
+    std::cout << digit << std::endl;
+  }
+  for (char ch : sv | std::views::reverse) {
+
   }
 }
 
 #ifdef DOCTEST_LIBRARY_INCLUDED
 
 TEST_CASE("[bigint] reeee") {
+  bigint a("-42");
   CHECK(1 == 1);
 }
 
