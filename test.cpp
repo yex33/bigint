3961bcab2e90d715baf7fe35c0d3ff86962704a9
Xunzhou Ye <yex33@mcmaster.ca>
2024-12-18 18:45:05 -0500

    poc: compile without doctest

--- test.cpp
+++ test.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "bigint.hpp"
+
 int main() {
   std::cout << "Hello world!" << std::endl;
+  bool res = bigint(12345) == bigint(12345);
+  std::cout << "should be 1: " << (int) res << std::endl;
 }
